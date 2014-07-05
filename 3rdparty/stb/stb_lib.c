60870cc9e0d1ef7a8f94de16164302e85249738f
vinjn <vinjn.z@gmail.com>
2014-07-05 23:17:30 +0800

    Build stb as a static library.

---
+++ 3rdparty/stb/stb_lib.c
@@ -0,0 +1,15 @@
+#define STB_DEFINE
+#include "stb/stb.h"
+#include "stb_vec.h"
+
+#define STB_DXT_IMPLEMENTATION
+#include "stb/stb_dxt.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+#define STB_PERLIN_IMPLEMENTATION
+#include "stb/stb_perlin.h"
