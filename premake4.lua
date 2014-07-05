5ee66ea52ba7bf692c2f53e4045bc3f22f4b7214
vinjn <vinjn.z@gmail.com>
2014-07-05 14:47:11 +0800

    Improve premake4.lua, add lua build.

--- premake4.lua
+++ premake4.lua
@@ -5,14 +5,25 @@ solution "island"
 	location ( "build" )
 	configurations { "Debug", "Release" }
 	platforms {"native", "x64", "x32"}
+    targetdir("build")
+    language "C"
+
+    configuration "vs*"
+        defines     { "_CRT_SECURE_NO_WARNINGS" }
+
+    configuration "Debug"
+        defines { "DEBUG" }
+        flags { "Symbols"}
+
+    configuration "Release"
+        defines { "NDEBUG" }
+        flags { "Optimize"}
 
     project "glfw"
-        language "C"
         kind "StaticLib"
         includedirs { "3rdparty/glfw/include" }
         files
         { 
-            "3rdparty/glfw/src/win32*.c",
             "3rdparty/glfw/src/clipboard.c",
             "3rdparty/glfw/src/context.c",
             "3rdparty/glfw/src/gamma.c",
@@ -21,79 +32,49 @@ solution "island"
             "3rdparty/glfw/src/joystick.c",
             "3rdparty/glfw/src/monitor.c",
             "3rdparty/glfw/src/time.c",
-            "3rdparty/glfw/src/wgl_context.c",
             "3rdparty/glfw/src/window.c",
-            "3rdparty/glfw/src/winmm_joystick.c",
             "3rdparty/glfw/src/input.c",
             "3rdparty/glfw/src/input.c",
         }
+
         targetdir("build")
+        defines { "_GLFW_USE_OPENGL" }
 
-        configuration "Debug"
-            defines { "DEBUG", "_GLFW_WIN32", "_GLFW_WGL", "_GLFW_USE_OPENGL" }
-            flags { "Symbols"}
-
-        configuration "Release"
-            defines { "NDEBUG", "_GLFW_WIN32", "_GLFW_WGL", "_GLFW_USE_OPENGL" }
-            flags { "Optimize"}
+        configuration "windows"
+            defines { "_GLFW_WIN32", "_GLFW_WGL" }
+            files 
+            {
+                "3rdparty/glfw/src/win32*.c",
+                "3rdparty/glfw/src/wgl_context.c",
+                "3rdparty/glfw/src/winmm_joystick.c",
+            }
 
     project "glew"
-        language "C"
         kind "StaticLib"
         includedirs { "3rdparty/glew" }
         files { "3rdparty/glew/*.c" }
-        targetdir("build")
-
-        configuration "Debug"
-            defines { "DEBUG", "GLEW_STATIC" }
-            flags { "Symbols", "ExtraWarnings"}
-
-        configuration "Release"
-            defines { "NDEBUG", "GLEW_STATIC" }
-            flags { "Optimize", "ExtraWarnings"}
+        defines "GLEW_STATIC"
 
     project "nanovg"
-        language "C"
         kind "StaticLib"
         includedirs { "3rdparty/nanovg/src" }
         files { "3rdparty/nanovg/src/*.c" }
-        targetdir("build")
-        --defines { "FONS_USE_FREETYPE" }   -- Uncomment to compile with FreeType support
-
-        configuration "Debug"
-            defines { "DEBUG" }
-            flags { "Symbols" }
-
-        configuration "Release"
-            defines { "NDEBUG" }
-            flags { "Optimize" }
 
     project "libuv"
-        language "C"
         kind "StaticLib"
         includedirs { "3rdparty/libuv/include" }
         files { "3rdparty/libuv/src/*.c", "3rdparty/libuv/src/win/*.c" }
-        targetdir("build")
 
-        configuration "Debug"
-            defines { "DEBUG" }
-            flags { "Symbols" }
+    project "lua"
+        os.copyfile("3rdparty/lua/src/luaconf.h.orig", "3rdparty/lua/src/luaconf.h")
+        kind "StaticLib"
+        includedirs { "3rdparty/lua/src" }
+        files { "3rdparty/lua/src/*.c"}
+        excludes
+        {
+            "3rdparty/lua/src/loadlib_rel.c",
+            "3rdparty/lua/src/lua.c",
+            "3rdparty/lua/src/luac.c",
+            "3rdparty/lua/src/print.c",
+        }
 
-        configuration "Release"
-            defines { "NDEBUG" }
-            flags { "Optimize" }
-
-    -- project "lua"
-    --     language "C"
-    --     kind "StaticLib"
-    --     includedirs { "3rdparty/lua/src" }
-    --     files { "3rdparty/lua/src/*.c"}
-    --     targetdir("build")
-
-    --     configuration "Debug"
-    --         defines { "DEBUG" }
-    --         flags { "Symbols" }
-
-    --     configuration "Release"
-    --         defines { "NDEBUG" }
-    --         flags { "Optimize" }
