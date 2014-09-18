877ac1237a09b6958b1c594fce191626b0fa3b6a
Vinjn Zhang <vinjn.z@gmail.com>
2014-09-18 17:09:05 +0800

    Add 02-audio-speech

---
+++ examples/02-audio-speech/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <soloud_c.h>
+
+int main(int parc, char ** pars)
+{
+    int i = 0;
+    Soloud *soloud = Soloud_create();
+    Speech *speech = Speech_create();
+
+    Speech_setText(speech, "1 2 3       A B C        Doooooo    Reeeeee    Miiiiii    Faaaaaa    Soooooo    Laaaaaa    Tiiiiii    Doooooo!");
+
+    Soloud_initEx(soloud,SOLOUD_CLIP_ROUNDOFF | SOLOUD_ENABLE_VISUALIZATION, SOLOUD_AUTO, SOLOUD_AUTO, SOLOUD_AUTO);
+
+    Soloud_setGlobalVolume(soloud, 4);
+    Soloud_play(soloud, speech);
+
+    printf("Playing..\n");
+
+    while (Soloud_getActiveVoiceCount(soloud) > 0)
+    {
+        float * v = Soloud_calcFFT(soloud);
+        int p = (int)(v[10] * 30);
+        if (p > 59) p = 59;
+        for (i = 0; i < p; i++)
+            printf("=");
+        for (i = p; i < 60; i++)
+            printf(" ");
+        printf("\r");
+        printf("%c\r", "|\\-/"[i&3]);
+        i++;
+    }
+    printf("\nFinished.\n");
+
+    Soloud_deinit(soloud);
+
+    Speech_destroy(speech);
+    Soloud_destroy(soloud);
+
+    printf("Cleanup done.\n");
+    return 0;
+}
