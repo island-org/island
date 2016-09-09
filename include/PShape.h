521fc59f9eb19a2955320cf04403326e4691e998
vinjn <vinjn.z@gmail.com>
2016-09-09 10:07:29 +0800

    Add placeholder of PShape.h

---
+++ include/PShape.h
@@ -0,0 +1,19 @@
+#ifndef PSHAPE_H
+#define PSHAPE_H
+
+
+typedef struct
+{
+
+} PShape;
+
+
+#endif // PSHAPE_H
+
+#ifdef PSHAPE_IMPLEMENTATION
+
+#define TINYOBJ_LOADER_C_IMPLEMENTATION
+#include "tinyobjloader-c/tinyobj_loader_c.h"
+
+#endif // PSHAPE_IMPLEMENTATION
+
