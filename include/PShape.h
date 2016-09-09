3fd0046cfb6611e4776b06ce6d9c72faea4603fe
vinjn <vinjn.z@gmail.com>
2016-09-09 10:18:50 +0800

    Adding functions.

--- include/PShape.h
+++ include/PShape.h
@@ -7,6 +7,9 @@ typedef struct
 
 } PShape;
 
+PShape loadShape(const char* filename);
+PShape createShape();
+void shape(PShape shp);
 
 #endif // PSHAPE_H
 
@@ -14,6 +17,25 @@ typedef struct
 
 #define TINYOBJ_LOADER_C_IMPLEMENTATION
 #include "tinyobjloader-c/tinyobj_loader_c.h"
+
+PShape loadShape(const char* filename)
+{
+    PShape shp;
+
+    return shp;
+}
+
+PShape createShape()
+{
+    PShape shp;
+
+    return shp;
+}
+
+void shape(PShape pshape)
+{
+    
+}
 
 #endif // PSHAPE_IMPLEMENTATION
 
